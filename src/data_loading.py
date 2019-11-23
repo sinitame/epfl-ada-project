fe8e3533ff311f1abacf128be8e34ece2663d310
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@grenoble-inp.org>
2019-11-23 16:58:41 +0100

    CSV path names exceptions handling

--- src/data_loading.py
+++ src/data_loading.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 class YearLoader:
     """
-    This class 
+    This class represents data for one year.
     
     Parameters
     ----------
@@ -19,9 +19,9 @@ class YearLoader:
         
     dataframes: dict
         Dictionnary containing all dataFrames for a year (keys in english)
-        
     
     """
+
     def __init__(self, year):
         self.default_encoding = "ISO-8859-1"
         self.default_mapping_csv = {
@@ -30,10 +30,24 @@ class YearLoader:
             "passengers"     : "usagers",
             "vehicles"       : "vehicules"
         }
+
+        separator = ""
+        delimiter = ""
+
+        # Handle exceptions in file paths
+        if int(year) <= 2016:
+            separator = "_"
+        else:
+            separator = "-"
         
+        if int(year) == 2009:
+            delimiter = "\t"
+        else:
+            delimiter = ","
+    
         self.dataframes = {}
         for k, v in self.default_mapping_csv.items():
-            self.dataframes.update({k:pd.read_csv("../data/{1}/{0}-{1}.csv".format(v, year), encoding=self.default_encoding)})
+            self.dataframes.update({k:pd.read_csv("../data/{2}/{0}{1}{2}.csv".format(v, separator, year), encoding=self.default_encoding, delimiter=delimiter)})
     
     def get_dataframe(self, name):
         return self.dataframes[name]
